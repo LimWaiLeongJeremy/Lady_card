4b0642d2df1858dee660e67e59603dc17174f48f
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-06-04 17:54:27 +0800

    added route for registerand import form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { GamePageComponent } from './component/game-page/game-page.component';
 import { DeckService } from "./service/deck.service";
 import { RegisterComponent } from './component/register/register.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 const routes: Routes = [
   {
     path: '',
+    title: 'Lady card',
+    component: RegisterComponent
+  },
+  {
+    path: 'game',
     title: 'Lady card',
     component: GamePageComponent
   },
@@ -29,7 +35,8 @@ const routes: Routes = [
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [
     DeckService
